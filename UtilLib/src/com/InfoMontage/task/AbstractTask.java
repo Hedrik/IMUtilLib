07864151b270fa5a69ffb6d63008a0894d092e12
hedrik <>
2006-08-13 04:49:27 +0000

    reformat of code; added joiner variable so that join() method is not empty (and won't get optimized away)

--- UtilLib/src/com/InfoMontage/task/AbstractTask.java
+++ UtilLib/src/com/InfoMontage/task/AbstractTask.java
@@ -22,7 +22,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *  
+ * 
  */
 
 package com.InfoMontage.task;
@@ -39,117 +39,125 @@ import com.InfoMontage.util.BooleanState;
  * @author Richard A. Mead <BR>
  *         Information Montage
  */
-public abstract class AbstractTask
-    implements Task
-{
+public abstract class AbstractTask implements Task {
 
     /**
-     * Logger for this class
-     */
+         * Logger for this class
+         */
     private static final AssertableLogger log = new AssertableLogger(
-        AbstractTask.class.getName());
+	    AbstractTask.class.getName());
 
-    static final CodeVersion implCodeVersion = new GenericCodeVersion(
-        "0.9.0.1b");
+    static final CodeVersion implCodeVersion = com.InfoMontage.version.GenericCodeVersion
+    .codeVersionFromCVSRevisionString("$Revision$");
 
     /**
-     * Comment for <code>params</code>
-     */
+         * Comment for <code>params</code>
+         */
     protected volatile Object[] params = null;
+
     protected volatile boolean paramsSet = false;
-    
+
     /**
-     * Comment for <code>processing</code>
-     */
+         * Comment for <code>processing</code>
+         */
     private BooleanState processing = new BooleanState(false);
 
+    private volatile boolean joiner = false;
+
     /**
-     *  
-     */
-    protected AbstractTask() {};
+         * 
+         */
+    protected AbstractTask() {
+    };
 
     /*
-     * (non-Javadoc)
-     * 
-     * @see com.InfoMontage.task.Task#isProcessing()
-     */
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#isProcessing()
+         */
     public boolean isProcessing() {
-        return processing.getState();
+	return processing.getState();
     }
 
     /*
-     * (non-Javadoc)
-     * 
-     * @see com.InfoMontage.task.Task#percentComplete()
-     */
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#percentComplete()
+         */
     public float percentComplete() {
-        // default is 0% until done
-        return (isProcessing() && paramsSet) ? 0.0f : 100.0f;
+	// default is 0% until done
+	return (isProcessing() && paramsSet) ? 0.0f : 100.0f;
     }
 
     /*
-     * (non-Javadoc)
-     * 
-     * @see com.InfoMontage.task.Task#processTask()
-     */
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#processTask()
+         */
     public synchronized void processTask() throws IllegalStateException {
-        if (this.paramsSet) {
-        processing.setState(true);
-        this.doTask();
-        this.params = null;
-        this.paramsSet=false;
-        processing.setState(false);
-        } else {
-            IllegalStateException e=new IllegalStateException("Attempt to"
-                +" process a Task that has not had it's parameters set first!");
-            assert (log.throwing(e));
-            throw e;
-        }
+	if (this.paramsSet) {
+	    processing.setState(true);
+	    this.doTask();
+	    this.params = null;
+	    this.paramsSet = false;
+	    processing.setState(false);
+	    this.joiner = false;
+	} else {
+	    IllegalStateException e = new IllegalStateException(
+		    "Attempt to process a Task that has not had it's parameters set first!");
+	    assert (log.throwing(e));
+	    throw e;
+	}
     }
 
     /**
-     * @throws IllegalStateException
-     */
+         * @throws IllegalStateException
+         */
     abstract protected void doTask() throws IllegalStateException;
 
     /*
-     * (non-Javadoc)
-     * 
-     * @see com.InfoMontage.task.Task#setTaskParameters(java.lang.Object[])
-     */
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#setTaskParameters(java.lang.Object[])
+         */
     public synchronized void setTaskParameters(Object[] pa)
-        throws IllegalArgumentException
-    {
-        Exception e = this.validateParameters(pa);
-        if (null != e) {
-            throw (IllegalArgumentException) new IllegalArgumentException(
-                "Attempt" + " to set a Task parameter to an illegal value!")
-                .initCause(e);
-        } else {
-            this.params = pa;
-            this.paramsSet=true;
-        }
+	    throws IllegalArgumentException {
+	Exception e = this.validateParameters(pa);
+	if (null != e) {
+	    throw (IllegalArgumentException) new IllegalArgumentException(
+		    "Attempt" + " to set a Task parameter to an illegal value!")
+		    .initCause(e);
+	} else {
+	    this.params = pa;
+	    this.paramsSet = true;
+	    this.joiner = false;
+	}
+    }
+
+    public synchronized void join() {
+	joiner = true;
     }
 
     /*
-     * (non-Javadoc)
-     * 
-     * @see com.InfoMontage.task.Task#clearTaskParameters()
-     */
-    public synchronized void clearTaskParameters()
-        throws IllegalStateException
-    {
-        if (this.processing.getState()) { throw new IllegalStateException(
-            "Attempt to clear a AbstractTask's parameters"
-                + " while AbstractTask is still processing!"); }
-        this.params = null;
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#clearTaskParameters()
+         */
+    public synchronized void clearTaskParameters() throws IllegalStateException {
+	if (this.processing.getState()) {
+	    throw new IllegalStateException(
+		    "Attempt to clear a AbstractTask's parameters"
+			    + " while AbstractTask is still processing!");
+	}
+	this.params = null;
+	this.joiner = false;
     }
 
     /**
-     * @param pa
-     * @return null if parameters are valid for the subclass, otherwise an
-     *         {@link Exception}explaining why validation failed.
-     */
+         * @param pa
+         * @return null if parameters are valid for the subclass, otherwise an
+         *         {@link Exception}explaining why validation failed.
+         */
     abstract protected Exception validateParameters(Object[] pa);
 
 }
