3011c18d4c2968aafdebc41116456f3cbca75c73
hedrik <>
2006-08-13 04:30:38 +0000

    Reformat or code only

--- UtilLib/src/com/InfoMontage/task/TaskQueue.java
+++ UtilLib/src/com/InfoMontage/task/TaskQueue.java
@@ -22,7 +22,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *  
+ * 
  */
 
 package com.InfoMontage.task;
@@ -36,7 +36,6 @@ import java.util.Vector;
 import com.InfoMontage.version.CodeVersion;
 import com.InfoMontage.version.GenericCodeVersion;
 
-
 /**
  * 
  * @author Richard A. Mead <BR>
@@ -45,174 +44,176 @@ import com.InfoMontage.version.GenericCodeVersion;
 public class TaskQueue {
 
     /**
-     * Logger for this class
-     */
+         * Logger for this class
+         */
     private static final AssertableLogger log = new AssertableLogger(
-        TaskQueue.class.getName());
+	    TaskQueue.class.getName());
 
-    public static CodeVersion implCodeVersion = new GenericCodeVersion(
-        "0.9");
+    public static CodeVersion implCodeVersion = new GenericCodeVersion("0.9");
 
     private Vector queue;
+
     private int minQueuesize;
+
     private int queueSizeIncrement;
+
     private int maxQueueSize;
 
     /**
-     * @param minimumQueueSize
-     * @param sizeIncrement
-     * @param maximumQueueSize
-     */
-    TaskQueue(int minimumQueueSize, int sizeIncrement, int maximumQueueSize)
-    {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue",
-                "TaskQueue(int minimumQueueSize = " + minimumQueueSize
-                    + ", int sizeIncrement = " + sizeIncrement
-                    + ", int maximumQueueSize = " + maximumQueueSize + ")",
-                "start of method");
-        }
+         * @param minimumQueueSize
+         * @param sizeIncrement
+         * @param maximumQueueSize
+         */
+    TaskQueue(int minimumQueueSize, int sizeIncrement, int maximumQueueSize) {
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue",
+		    "TaskQueue(int minimumQueueSize = " + minimumQueueSize
+			    + ", int sizeIncrement = " + sizeIncrement
+			    + ", int maximumQueueSize = " + maximumQueueSize
+			    + ")", "start of method");
+	}
 
-        minQueuesize = minimumQueueSize;
-        queueSizeIncrement = sizeIncrement;
-        queue = new Vector(minimumQueueSize, sizeIncrement);
-        maxQueueSize = maximumQueueSize;
+	minQueuesize = minimumQueueSize;
+	queueSizeIncrement = sizeIncrement;
+	queue = new Vector(minimumQueueSize, sizeIncrement);
+	maxQueueSize = maximumQueueSize;
 
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue",
-                "TaskQueue(int minimumQueueSize = " + minimumQueueSize
-                    + ", int sizeIncrement = " + sizeIncrement
-                    + ", int maximumQueueSize = " + maximumQueueSize + ")",
-                "end of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue",
+		    "TaskQueue(int minimumQueueSize = " + minimumQueueSize
+			    + ", int sizeIncrement = " + sizeIncrement
+			    + ", int maximumQueueSize = " + maximumQueueSize
+			    + ")", "end of method");
+	}
     }
 
     /**
-     * @param q
-     */
+         * @param q
+         */
     TaskQueue(final TaskQueue q) {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue",
-                "TaskQueue(TaskQueue q = " + q + ")", "start of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue",
+		    "TaskQueue(TaskQueue q = " + q + ")", "start of method");
+	}
 
-        queue = new Vector(q.queue.capacity(), q.queueSizeIncrement);
-        maxQueueSize = q.maxQueueSize;
+	queue = new Vector(q.queue.capacity(), q.queueSizeIncrement);
+	maxQueueSize = q.maxQueueSize;
 
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue",
-                "TaskQueue(TaskQueue q = " + q + ")", "end of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue",
+		    "TaskQueue(TaskQueue q = " + q + ")", "end of method");
+	}
     }
 
     boolean enqueue(Task t) {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue",
-                "enqueue(Task t = " + t + ")", "start of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue", "enqueue(Task t = "
+		    + t + ")", "start of method");
+	}
 
-        boolean queued = false;
-        try {
-            queued = enqueue(t, false);
-        } catch (InterruptedException e) {
-            if (log.isLoggable(Level.FINER)) {
-                log.throwing("com.InfoMontage.task.TaskQueue",
-                    "enqueue(Task t = " + t + ")", e);
-            }
+	boolean queued = false;
+	try {
+	    queued = enqueue(t, false);
+	} catch (InterruptedException e) {
+	    if (log.isLoggable(Level.FINER)) {
+		log.throwing("com.InfoMontage.task.TaskQueue",
+			"enqueue(Task t = " + t + ")", e);
+	    }
 
-            // cannot happen since we called with blocking=false
-            e.printStackTrace();
-        }
+	    // cannot happen since we called with blocking=false
+	    e.printStackTrace();
+	}
 
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue",
-                "enqueue(Task t = " + t + ")",
-                "end of method - return value = " + queued);
-        }
-        return queued;
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue", "enqueue(Task t = "
+		    + t + ")", "end of method - return value = " + queued);
+	}
+	return queued;
     }
 
     synchronized boolean enqueue(Task t, boolean blocking)
-        throws InterruptedException
-    {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue",
-                "enqueue(Task t = " + t + ", boolean blocking = "
-                    + blocking + ")", "start of method");
-        }
+	    throws InterruptedException {
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue", "enqueue(Task t = "
+		    + t + ", boolean blocking = " + blocking + ")",
+		    "start of method");
+	}
 
-        boolean queued = false;
-        synchronized (queue) {
-            while (maxQueueSize <= queue.size()) {
-                if (!blocking)
-                    break;
-                else
-                    queue.wait();
-            }
-        }
-        queued = queue.add(t);
+	boolean queued = false;
 
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue",
-                "enqueue(Task t = " + t + ", boolean blocking = "
-                    + blocking + ")", "end of method - return value = "
-                    + queued);
-        }
-        return queued;
+	if (t.isProcessing()) {
+	    log.severe("com.InfoMontage.task.TaskQueue attempt to enqeue a"
+		    + "task that is still processing: " + t);
+	} else {
+	    synchronized (queue) {
+		while (maxQueueSize <= queue.size()) {
+		    if (!blocking)
+			break;
+		    else
+			queue.wait();
+		}
+		queued = queue.add(t);
+	    }
+	}
+
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue", "enqueue(Task t = "
+		    + t + ", boolean blocking = " + blocking + ")",
+		    "end of method - return value = " + queued);
+	}
+	return queued;
     }
 
     Task dequeue() {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue", "dequeue()",
-                "start of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue", "dequeue()",
+		    "start of method");
+	}
 
-        Task t = null;
-        synchronized (queue) {
-            t = (Task) queue.firstElement();
-            queue.remove(t);
-            queue.notify();
-        }
+	Task t = null;
+	synchronized (queue) {
+	    t = (Task) queue.firstElement();
+	    queue.remove(t);
+	    queue.notify();
+	}
 
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue", "dequeue()",
-                "end of method - return value = " + t);
-        }
-        return t;
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue", "dequeue()",
+		    "end of method - return value = " + t);
+	}
+	return t;
     }
 
     /**
-     * @return Returns the maximum queue size.
-     */
+         * @return Returns the maximum queue size.
+         */
     int getMaxQueueSize() {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue",
-                "getMaxQueueSize()", "start of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue", "getMaxQueueSize()",
+		    "start of method");
+	}
 
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue",
-                "getMaxQueueSize()", "end of method - return value = "
-                    + maxQueueSize);
-        }
-        return maxQueueSize;
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue", "getMaxQueueSize()",
+		    "end of method - return value = " + maxQueueSize);
+	}
+	return maxQueueSize;
     }
 
     /**
-     * @see java.lang.Object#toString()
-     */
+         * @see java.lang.Object#toString()
+         */
     public synchronized String toString() {
-        if (log.isLoggable(Level.FINER)) {
-            log.entering("com.InfoMontage.task.TaskQueue", "toString()",
-                "start of method");
-        }
+	if (log.isLoggable(Level.FINER)) {
+	    log.entering("com.InfoMontage.task.TaskQueue", "toString()",
+		    "start of method");
+	}
 
-
-        String returnString = super.toString();
-        if (log.isLoggable(Level.FINER)) {
-            log.exiting("com.InfoMontage.task.TaskQueue", "toString()",
-                "end of method - return value = " + returnString);
-        }
-        return returnString;
+	String returnString = super.toString();
+	if (log.isLoggable(Level.FINER)) {
+	    log.exiting("com.InfoMontage.task.TaskQueue", "toString()",
+		    "end of method - return value = " + returnString);
+	}
+	return returnString;
     }
 }
