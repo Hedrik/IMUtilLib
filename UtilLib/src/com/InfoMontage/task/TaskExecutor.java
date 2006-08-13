0a9021b12689a2dcc36074eb6540c321919dda05
hedrik <>
2006-08-13 04:47:16 +0000

    reformat of code; added call to 'stopRunning' if thread is interrupted; added 'yield' call to allow new TaskExecutors a chance to actually run

--- UtilLib/src/com/InfoMontage/task/TaskExecutor.java
+++ UtilLib/src/com/InfoMontage/task/TaskExecutor.java
@@ -27,15 +27,10 @@
 
 package com.InfoMontage.task;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import com.InfoMontage.util.AssertableLogger;
-
 import com.InfoMontage.util.BooleanState;
 import com.InfoMontage.version.CodeVersion;
 import com.InfoMontage.version.GenericCodeVersion;
-
-import java.util.Stack;
 
 /**
  * NOTE: made public solely for purposes of javadoc type resolution.
@@ -43,150 +38,150 @@ import java.util.Stack;
  * @author Richard A. Mead <BR>
  *         Information Montage
  */
-public final class TaskExecutor
-    extends Thread
-{
+public final class TaskExecutor extends Thread {
 
     /**
-     * Logger for this class
-     */
+         * Logger for this class
+         */
     private static final AssertableLogger log = new AssertableLogger(
-        TaskExecutor.class.getName());
+	    TaskExecutor.class.getName());
 
-    public static CodeVersion implCodeVersion = new GenericCodeVersion(
-        "0.7");
+    public static CodeVersion implCodeVersion = com.InfoMontage.version.GenericCodeVersion
+    .codeVersionFromCVSRevisionString("$Revision$");
 
     private final BooleanState running = new BooleanState(false);
+
     private final BooleanState validExecutor = new BooleanState(false);
+
     private final TaskExecutorPool myFactory;
+
     private final BooleanState waiting = new BooleanState(false);
+
     private volatile Task myTask = null;
 
     /** Creates a new instance of a TaskExecutor - package access only */
-    TaskExecutor(TaskExecutorPool factory, ThreadGroup group, int stackSize)
-    {
-        super(group, null, "Task processor", stackSize);
-        assert (log.entering("com.InfoMontage.task.TaskExecutor",
-            "TaskExecutor(TaskExecutorPool factory = " + factory
-                + ", ThreadGroup group = " + group + ", int stackSize = "
-                + stackSize + ")", "start of method"));
+    TaskExecutor(TaskExecutorPool factory, ThreadGroup group, int stackSize) {
+	super(group, null, "Task processor", stackSize);
+	assert (log.entering("com.InfoMontage.task.TaskExecutor",
+		"TaskExecutor(TaskExecutorPool factory = " + factory
+			+ ", ThreadGroup group = " + group
+			+ ", int stackSize = " + stackSize + ")",
+		"start of method"));
 
-        if (null == factory) {
-            IllegalArgumentException e = (IllegalArgumentException) new IllegalArgumentException(
-                "Attempt to create a TaskExecutor with a null TaskExecutorPool!")
-                .initCause(new NullPointerException());
-            assert (log.throwing(e));
-            throw e;
-        }
-        myFactory = factory;
+	if (null == factory) {
+	    IllegalArgumentException e = (IllegalArgumentException) new IllegalArgumentException(
+		    "Attempt to create a TaskExecutor with a null TaskExecutorPool!")
+		    .initCause(new NullPointerException());
+	    assert (log.throwing(e));
+	    throw e;
+	}
+	myFactory = factory;
 
-        assert (log.exiting("com.InfoMontage.task.TaskExecutor",
-            "TaskExecutor(TaskExecutorPool factory = " + factory
-                + ", ThreadGroup group = " + group + ", int stackSize = "
-                + stackSize + ")", "end of method"));
+	assert (log.exiting("com.InfoMontage.task.TaskExecutor",
+		"TaskExecutor(TaskExecutorPool factory = " + factory
+			+ ", ThreadGroup group = " + group
+			+ ", int stackSize = " + stackSize + ")",
+		"end of method"));
     }
 
     synchronized void validateTaskThread() {
-        assert (log.finer("Validating TaskExecutor."));
-        validExecutor.setState(true);
-        assert (log.finer("TaskExecutor Validated."));
+	assert (log.finer("Validating TaskExecutor."));
+	validExecutor.setState(true);
+	assert (log.finer("TaskExecutor Validated."));
     }
 
-    synchronized void executeTask(Task t)
-        throws IllegalStateException, IllegalMonitorStateException
-    {
-        assert (log.entering("com.InfoMontage.task.TaskExecutor",
-            "executeTask(Task t = " + t + ")", "start of method"));
+    synchronized void executeTask(Task t) throws IllegalStateException,
+	    IllegalMonitorStateException {
+	assert (log.entering("com.InfoMontage.task.TaskExecutor",
+		"executeTask(Task t = " + t + ")", "start of method"));
 
-        if (!validExecutor.getState()) {
-            IllegalStateException e = new IllegalStateException(
-                "Attempt to use a TaskExecutor that is still a member of the"
-                    + " TaskExecutorPool's TaskExecutor pool!");
-            assert (log.throwing(e));
-            throw e;
-        }
-        if (!waiting.getState()) {
-            IllegalStateException e = new IllegalStateException(
-                "Attempt to use a TaskExecutor that"
-                    + " is already executing a Task!");
-            assert (log.throwing(e));
-            throw e;
-        }
-        if (!this.running.getState()) {
-            IllegalStateException e = new IllegalStateException(
-                "Attempt to use a TaskExecutor that"
-                    + " has not been started!");
-            assert (log.throwing(e));
-            throw e;
-        }
-        myTask = t;
-        synchronized (this.waiting) {
-            this.waiting.notify();
-        }
+	if (!validExecutor.getState()) {
+	    IllegalStateException e = new IllegalStateException(
+		    "Attempt to use a TaskExecutor that is still a member of the"
+			    + " TaskExecutorPool's TaskExecutor pool!");
+	    assert (log.throwing(e));
+	    throw e;
+	}
+	if (!waiting.getState()) {
+	    IllegalStateException e = new IllegalStateException(
+		    "Attempt to use a TaskExecutor that"
+			    + " is already executing a Task!");
+	    assert (log.throwing(e));
+	    throw e;
+	}
+	if (!this.running.getState()) {
+	    IllegalStateException e = new IllegalStateException(
+		    "Attempt to use a TaskExecutor that"
+			    + " has not been started!");
+	    assert (log.throwing(e));
+	    throw e;
+	}
+	myTask = t;
+	synchronized (this.waiting) {
+	    this.waiting.notify();
+	}
+	Thread.yield();
 
-        assert (log.exiting("com.InfoMontage.task.TaskExecutor",
-            "executeTask(Task t = " + t + ")", "end of method"));
+	assert (log.exiting("com.InfoMontage.task.TaskExecutor",
+		"executeTask(Task t = " + t + ")", "end of method"));
     }
 
     synchronized boolean isWaiting() {
-        assert (log.entering("start of method"));
+	assert (log.entering("start of method"));
 
-        boolean returnboolean = waiting.getState();
-        assert (log.exiting("end of method - return value = "
-            + returnboolean));
-        return returnboolean;
+	boolean returnboolean = waiting.getState();
+	assert (log.exiting("end of method - return value = " + returnboolean));
+	return returnboolean;
     }
 
     public void run() throws IllegalStateException {
-        assert (log.entering("start of method"));
+	assert (log.entering("start of method"));
 
-        if (running.getState()) {
-            IllegalStateException e = new IllegalStateException(
-                "Attempt to run a ThreadTask that" + " is already running!");
-            assert (log.throwing(e));
-            throw e;
-        }
-        running.setState(true);
-        while (running.getState()) {
-            try {
-                assert (log.gettingLock(waiting));
-                synchronized (waiting) {
-                    assert (log.gotLock(waiting));
-                    waiting.setState(true);
-                    waiting.wait();
-                    waiting.setState(false);
-                    assert (log.releasedLock(waiting));
-                }
-            } catch (InterruptedException e) {
-                assert (log.throwing(e));
-                throw (RuntimeException) new RuntimeException()
-                    .initCause(e);
-            }
-            assert (log.gettingLock(validExecutor));
-            synchronized (validExecutor) {
-                assert (log.gotLock(validExecutor));
-                if (myTask != null) {
-                    myTask.processTask();
-                }
-                myTask = null;
-                validExecutor.setState(false);
-                myFactory.returnTaskExecutor(this);
-                assert (log.releasedLock(validExecutor));
-            }
-        }
+	if (running.getState()) {
+	    IllegalStateException e = new IllegalStateException(
+		    "Attempt to run a ThreadTask that" + " is already running!");
+	    assert (log.throwing(e));
+	    throw e;
+	}
+	running.setState(true);
+	while (running.getState()) {
+	    try {
+		assert (log.gettingLock(waiting));
+		synchronized (waiting) {
+		    assert (log.gotLock(waiting));
+		    waiting.setState(true);
+		    waiting.wait();
+		    waiting.setState(false);
+		    assert (log.releasedLock(waiting));
+		}
+	    } catch (InterruptedException e) {
+		assert (log.throwing(e));
+		this.stopRunning();
+		throw (RuntimeException) new RuntimeException().initCause(e);
+	    }
+	    assert (log.gettingLock(validExecutor));
+	    synchronized (validExecutor) {
+		assert (log.gotLock(validExecutor));
+		if (myTask != null) {
+		    myTask.processTask();
+		}
+		myTask = null;
+		validExecutor.setState(false);
+		myFactory.returnTaskExecutor(this);
+		assert (log.releasedLock(validExecutor));
+	    }
+	}
 
-        assert (log.exiting("end of method"));
+	assert (log.exiting("end of method"));
     }
 
-    public synchronized void stopRunning()
-        throws IllegalMonitorStateException
-    {
-        assert (log.entering("start of method"));
+    public synchronized void stopRunning() throws IllegalMonitorStateException {
+	assert (log.entering("start of method"));
 
-        running.setState(false);
-        waiting.notify();
+	running.setState(false);
+	waiting.notify();
 
-        assert (log.exiting("end of method"));
+	assert (log.exiting("end of method"));
     }
 
 }
