8c754ba29a8134dbf060c964a8373cff963a6d8b
hedrik <>
2006-08-13 04:59:31 +0000

    reformatted code; added integer to Roman numeral converter

--- UtilLib/src/com/InfoMontage/util/Math.java
+++ UtilLib/src/com/InfoMontage/util/Math.java
@@ -22,7 +22,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *  
+ * 
  */
 
 package com.InfoMontage.util;
@@ -37,17 +37,42 @@ import com.InfoMontage.math.BigCounter;
 public final class Math {
 
     public static final BigInteger MAX_LONG_PLUS_ONE = BigInteger.valueOf(
-        Long.MAX_VALUE).add(BigInteger.ONE);
+	    Long.MAX_VALUE).add(BigInteger.ONE);
+
+    private static int[] romanNumbers = { 1000, 900, 500, 400, 100, 90, 50, 40,
+	    10, 9, 5, 4, 1 };
+
+    private static String[] romanLetters = { "M", "CM", "D", "CD", "C", "XC",
+	    "L", "XL", "X", "IX", "V", "IV", "I" };
 
     /** Cannot create an instance of Math - static utility functions only */
-    private Math() {}
-
-    public static final long modLong(BigCounter bc) {
-        return modLong(bc.get());
+    private Math() {
     }
 
-    public static final long modLong(BigInteger bi) {
-        return (bi.mod(MAX_LONG_PLUS_ONE)).longValue();
+    public static long modLong(final BigCounter bc) {
+	return modLong(bc.get());
     }
 
+    public static long modLong(final BigInteger bi) {
+	return (bi.mod(MAX_LONG_PLUS_ONE)).longValue();
+    }
+
+    /**
+         * Return the standard Roman numeral representation of an integer.
+         * 
+         * @param num
+         * @return the standard Roman numeral representation of the integer num.
+         */
+    public static String intToRoman(final int num) {
+	StringBuffer roman = new StringBuffer(); // The roman numeral.
+	int n = num; // N represents the part of num that still has
+	// to be converted to Roman numeral representation.
+	for (int i = 0; i < romanNumbers.length; i++) {
+	    while (n >= romanNumbers[i]) {
+		roman.append(romanLetters[i]);
+		n -= romanNumbers[i];
+	    }
+	}
+	return roman.toString();
+    }
 }
