5f12659acfc7b2a0359c00205073126bf91f18e0
hedrik <>
2006-08-16 06:48:16 +0000

    reformat of code; changed joiner variable to be a BooleanState object so it could be used for synchronization; modified join() method to use synchronization for the join() logic; added missing calls to join() prior to clearing task parameters in doMultiTask() test - thus allowing it to succeed in certain cases where thread timing was not allowing the Tasks to complete prior to the clearTaskParameters() call.

--- UtilLib/src/com/InfoMontage/task/TaskExecutorPool_JUnitTest.java
+++ UtilLib/src/com/InfoMontage/task/TaskExecutorPool_JUnitTest.java
@@ -27,6 +27,8 @@
 
 package com.InfoMontage.task;
 
+import com.InfoMontage.util.BooleanState;
+
 import junit.framework.TestCase;
 
 /**
@@ -36,233 +38,259 @@ import junit.framework.TestCase;
  */
 public class TaskExecutorPool_JUnitTest extends TestCase {
 
-	volatile TaskExecutorPool p;
-	volatile Task t1;
-	volatile Task t2;
-	
-	public static void main(String[] args) {
-		junit.swingui.TestRunner.run(TaskExecutorPool_JUnitTest.class);
-	}
+    volatile TaskExecutorPool p;
 
-	/**
-	 * Constructor for TaskExecutorPool_JUnitTest.
-	 * 
-	 * @param arg0
-	 */
-	public TaskExecutorPool_JUnitTest(String arg0) {
-		super(arg0);
-	}
+    volatile Task t1;
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	protected void setUp() throws Exception {
-		super.setUp();
-		this.p = TaskExecutorPool.getPool();
-		this.t1 = new TaskExecutorPoolTestTask();
-		this.t2 = new TaskExecutorPoolTestTask();
-	}
+    volatile Task t2;
 
-	/* (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		this.p = null;
-		this.t1 = null;
-		this.t2 = null;		
-		super.tearDown();
-	}
+    public static void main(String[] args) {
+	junit.swingui.TestRunner.run(TaskExecutorPool_JUnitTest.class);
+    }
 
-	public static class TaskExecutorPoolTestTask implements Task {
+    /**
+         * Constructor for TaskExecutorPool_JUnitTest.
+         * 
+         * @param arg0
+         */
+    public TaskExecutorPool_JUnitTest(String arg0) {
+	super(arg0);
+    }
 
-		volatile boolean parmsSet = false;
+    /*
+         * (non-Javadoc)
+         * 
+         * @see junit.framework.TestCase#setUp()
+         */
+    protected void setUp() throws Exception {
+	super.setUp();
+	this.p = TaskExecutorPool.getPool();
+	this.t1 = new TaskExecutorPoolTestTask();
+	this.t2 = new TaskExecutorPoolTestTask();
+    }
 
-		volatile boolean taskDone = true;
+    /*
+         * (non-Javadoc)
+         * 
+         * @see junit.framework.TestCase#tearDown()
+         */
+    protected void tearDown() throws Exception {
+	this.p = null;
+	this.t1 = null;
+	this.t2 = null;
+	super.tearDown();
+    }
 
-		boolean joiner = false;
+    public static class TaskExecutorPoolTestTask implements Task {
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.InfoMontage.task.Task#processTask()
-		 */
-		public synchronized void processTask() throws IllegalStateException {
-			if (parmsSet)
-				taskDone = true;
-			else
-				throw new IllegalStateException(
-						"Task parameters not set prior to calling processTask()!");
-			joiner = false;
-		}
+	volatile boolean parmsSet = false;
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.InfoMontage.task.Task#isProcessing()
-		 */
-		public boolean isProcessing() {
-			return (parmsSet && !taskDone);
-		}
+	volatile boolean taskDone = true;
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.InfoMontage.task.Task#percentComplete()
-		 */
-		public float percentComplete() {
-			return (isProcessing()) ? 0 : 100;
-		}
+	volatile BooleanState joiner = new BooleanState(false);
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.InfoMontage.task.Task#setTaskParameters(java.lang.Object[])
-		 */
-		public void setTaskParameters(Object[] pa)
-				throws IllegalArgumentException, IllegalStateException {
-			if (this.isProcessing()) {
-				throw new IllegalStateException(
-						"Task parameters being set while still processing!");
-			} else {
-				if ((null != pa) && (pa.length == 1) && (null == pa[0]))
-					parmsSet = true;
-				else
-					throw new IllegalArgumentException(
-							"Task parameters not being set to a valid value!");
-				taskDone = false;
-			}
-		}
-
-		public synchronized void join() {
-			joiner = true;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.InfoMontage.task.Task#clearTaskParameters()
-		 */
-		public void clearTaskParameters() throws IllegalStateException {
-			if (!this.isProcessing())
-				parmsSet = false;
-			else
-				throw new IllegalStateException(
-						"Task parameters being cleared while still processing task!");
-		}
-
+	/*
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#processTask()
+         */
+	public synchronized void processTask() throws IllegalStateException {
+	    synchronized (joiner) {
+		if (parmsSet)
+		    taskDone = true;
+		else
+		    throw new IllegalStateException(
+			    "Task parameters not set prior to calling processTask()!");
+		joiner.setState(false);
+		joiner.notifyAll();
+	    }
 	}
 
 	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool()'
-	 */
-	public void testGetPool() {
-		TaskExecutorPool p = TaskExecutorPool.getPool();
-		assertNotNull(p);
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#isProcessing()
+         */
+	public boolean isProcessing() {
+	    return (parmsSet && !taskDone);
 	}
 
 	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool(String)'
-	 */
-	public void testGetPoolString() {
-		TaskExecutorPool p = TaskExecutorPool.getPool("TestPool");
-		assertNotNull(p);
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#percentComplete()
+         */
+	public float percentComplete() {
+	    return (isProcessing()) ? 0 : 100;
 	}
 
 	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool(String,
-	 * int)'
-	 */
-	public void testGetPoolStringInt() {
-		TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2);
-		assertNotNull(p);
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#setTaskParameters(java.lang.Object[])
+         */
+	public void setTaskParameters(Object[] pa)
+		throws IllegalArgumentException, IllegalStateException {
+	    if (this.isProcessing()) {
+		throw new IllegalStateException(
+			"Task parameters being set while still processing!");
+	    } else {
+		if ((null != pa) && (pa.length == 1) && (null == pa[0]))
+		    parmsSet = true;
+		else
+		    throw new IllegalArgumentException(
+			    "Task parameters not being set to a valid value!");
+		taskDone = false;
+	    }
 	}
 
-	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool(String,
-	 * int, int)'
-	 */
-	public void testGetPoolStringIntInt() {
-		TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2, 5);
-		assertNotNull(p);
-	}
-
-	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool(String,
-	 * int, int, boolean)'
-	 */
-	public void testGetPoolStringIntIntBoolean() {
-		TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2, 5, false);
-		assertNotNull(p);
-	}
-
-	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool(String,
-	 * int, int, boolean, int)'
-	 */
-	public void testGetPoolStringIntIntBooleanInt() {
-		TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2, 5, false,
-				42);
-		assertNotNull(p);
-	}
-
-	/*
-	 * /* Test method for 'com.InfoMontage.task.TaskExecutorPool.doTask(Task,
-	 * boolean)'
-	 */
-	public void testDoMultiTask() {
-		try {
-			t1.setTaskParameters(new Object[] { null });
-			p.doTask(t1, false);
-			t2.setTaskParameters(new Object[] { null });
-			p.doTask(t2, false);
-			t1.join();
-			t1.clearTaskParameters();
-			t2.join();
-			t2.clearTaskParameters();
-
-			t1.setTaskParameters(new Object[] { null });
-			p.doTask(t1, false);
-			t1.join();
-			t1.clearTaskParameters();
-			t2.setTaskParameters(new Object[] { null });
-			p.doTask(t2, false);
-			t2.join();
-			t2.clearTaskParameters();
-
-			t1.setTaskParameters(new Object[] { null });
-			t2.setTaskParameters(new Object[] { null });
-			p.doTask(t1, false);
-			p.doTask(t2, false);
-			t1.clearTaskParameters();
-			t2.clearTaskParameters();
-
-			t1.setTaskParameters(new Object[] { null });
-			t2.setTaskParameters(new Object[] { null });
-			p.doTask(t1, false);
-			t1.clearTaskParameters();
-			p.doTask(t2, false);
-			t2.clearTaskParameters();
-
-		} catch (Exception e) {
-			fail(e.toString());
+	public void join() {
+	    synchronized (joiner) {
+		joiner.setState(true);
+		while (this.isProcessing()) {
+		    try {
+			this.joiner.wait();
+		    } catch (InterruptedException e) {
+			throw (RuntimeException) new RuntimeException()
+				.initCause(e);
+		    }
 		}
+	    }
 	}
 
 	/*
-	 * Test method for 'com.InfoMontage.task.TaskExecutorPool.doTask(Task,
-	 * boolean)'
-	 */
-	public void testDoTask() {
-		TaskExecutorPool p = TaskExecutorPool.getPool();
-		Task t = new TaskExecutorPoolTestTask();
-		try {
-			t.setTaskParameters(new Object[] { null });
-			p.doTask(t, false);
-			t.join();
-			t.clearTaskParameters();
-		} catch (Exception e) {
-			fail(e.toString());
-		}
+         * (non-Javadoc)
+         * 
+         * @see com.InfoMontage.task.Task#clearTaskParameters()
+         */
+	public void clearTaskParameters() throws IllegalStateException {
+	    if (!this.isProcessing())
+		parmsSet = false;
+	    else
+		throw new IllegalStateException(
+			"Task parameters being cleared while still processing task!");
 	}
+
+    }
+
+    /*
+         * Test method for 'com.InfoMontage.task.TaskExecutorPool.getPool()'
+         */
+    public void testGetPool() {
+	TaskExecutorPool p = TaskExecutorPool.getPool();
+	assertNotNull(p);
+    }
+
+    /*
+         * Test method for
+         * 'com.InfoMontage.task.TaskExecutorPool.getPool(String)'
+         */
+    public void testGetPoolString() {
+	TaskExecutorPool p = TaskExecutorPool.getPool("TestPool");
+	assertNotNull(p);
+    }
+
+    /*
+         * Test method for
+         * 'com.InfoMontage.task.TaskExecutorPool.getPool(String, int)'
+         */
+    public void testGetPoolStringInt() {
+	TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2);
+	assertNotNull(p);
+    }
+
+    /*
+         * Test method for
+         * 'com.InfoMontage.task.TaskExecutorPool.getPool(String, int, int)'
+         */
+    public void testGetPoolStringIntInt() {
+	TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2, 5);
+	assertNotNull(p);
+    }
+
+    /*
+         * Test method for
+         * 'com.InfoMontage.task.TaskExecutorPool.getPool(String, int, int,
+         * boolean)'
+         */
+    public void testGetPoolStringIntIntBoolean() {
+	TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2, 5, false);
+	assertNotNull(p);
+    }
+
+    /*
+         * Test method for
+         * 'com.InfoMontage.task.TaskExecutorPool.getPool(String, int, int,
+         * boolean, int)'
+         */
+    public void testGetPoolStringIntIntBooleanInt() {
+	TaskExecutorPool p = TaskExecutorPool.getPool("TestPool", 2, 5, false,
+		42);
+	assertNotNull(p);
+    }
+
+    /*
+         * /* Test method for
+         * 'com.InfoMontage.task.TaskExecutorPool.doTask(Task, boolean)'
+         */
+    public void testDoMultiTask() {
+	try {
+	    t1.setTaskParameters(new Object[] { null });
+	    p.doTask(t1, false);
+	    t2.setTaskParameters(new Object[] { null });
+	    p.doTask(t2, false);
+	    t1.join();
+	    t1.clearTaskParameters();
+	    t2.join();
+	    t2.clearTaskParameters();
+
+	    t1.setTaskParameters(new Object[] { null });
+	    p.doTask(t1, false);
+	    t1.join();
+	    t1.clearTaskParameters();
+	    t2.setTaskParameters(new Object[] { null });
+	    p.doTask(t2, false);
+	    t2.join();
+	    t2.clearTaskParameters();
+
+	    t1.setTaskParameters(new Object[] { null });
+	    t2.setTaskParameters(new Object[] { null });
+	    p.doTask(t1, false);
+	    p.doTask(t2, false);
+	    t1.join();
+	    t2.join();
+	    t1.clearTaskParameters();
+	    t2.clearTaskParameters();
+
+	    t1.setTaskParameters(new Object[] { null });
+	    t2.setTaskParameters(new Object[] { null });
+	    p.doTask(t1, false);
+	    t1.join();
+	    t1.clearTaskParameters();
+	    p.doTask(t2, false);
+	    t2.join();
+	    t2.clearTaskParameters();
+
+	} catch (Exception e) {
+	    fail(e.toString());
+	}
+    }
+
+    /*
+         * Test method for 'com.InfoMontage.task.TaskExecutorPool.doTask(Task,
+         * boolean)'
+         */
+    public void testDoTask() {
+	TaskExecutorPool p = TaskExecutorPool.getPool();
+	Task t = new TaskExecutorPoolTestTask();
+	try {
+	    t.setTaskParameters(new Object[] { null });
+	    p.doTask(t, false);
+	    t.join();
+	    t.clearTaskParameters();
+	} catch (Exception e) {
+	    fail(e.toString());
+	}
+    }
 
 }
